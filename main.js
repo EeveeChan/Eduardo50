9f3d26f5277bac58fc78d330839b523f43fb3cc5
EeveeChan <103970972+EeveeChan@users.noreply.github.com>
2022-12-06 16:04:11 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function evento(){
+document.getElementById("imagem").src="https://i0.wp.com/panelaterapia.com/wp-content/uploads/2016/11/panquecas-receita.jpg?resize=640%2C576"
+}
+function evento1(){
+  document.getElementById("link").style.fontSize="20px"
+}
+function evento2(){
+  document.getElementById("link").style.fontSize="15px"
+}
